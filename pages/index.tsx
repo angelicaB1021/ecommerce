eb0973c5d5ade822982e39f39421493f27868f01
rosa angelica beltran <rbeltran3@misena.edu.co>
2024-05-26 16:48:47 -0500

    actualizacion de botores y estructuras

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { Typography } from '@mui/material'
+import { ShopLayout } from '../components/layouts'
 
-const inter = Inter({ subsets: ["latin"] });
+
+
 
 export default function Home() {
   return (
-    <>
-      <h1> Ecommerce Adso </h1>
-    </>
-  );
+    <ShopLayout titile={'Ecommerce Sena - Home'} pageDescription={'Encuentra los mejores articulos para tu setup'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Articulos</Typography>
+    </ShopLayout>
+  )
 }
