5bf86c6cda912506f96fbcdacc9c0b9028f36e55
rosa angelica beltran <rbeltran3@misena.edu.co>
2024-05-30 16:39:43 -0500

    Add: component productCard and productList

--- components/iu/SideMenu.tsx
+++ components/iu/SideMenu.tsx
@@ -4,7 +4,7 @@ import { Box, Drawer, Input, List, ListItem } from "@mui/material"
  export const SideMenu = () => {
   return (
     <Drawer
-    open={true}
+    open={false}
     anchor='right'
     sx={{backdropFilter: 'plur(4px)', transition: 'all 0.5s ease-out' }}
     >
