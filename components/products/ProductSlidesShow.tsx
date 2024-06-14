b61c4c9abc98000b6eff38bdcd0b09c9edb3cf15
rosa angelica beltran <rbeltran3@misena.edu.co>
2024-06-14 16:21:19 -0500

    error en slug y en productsledesshow

--- components/products/ProductSlidesShow.tsx
+++ components/products/ProductSlidesShow.tsx
@@ -5,32 +5,30 @@ import 'react-slideshow-image/dist/styles.css';
 import styles from './ProductSlideshow.module.css';
 
 interface Props {
-    images: string[]
+  images: string[]
 }
 
 export const ProductSlidesShow: FC<Props> = ({ images }) => {
   return (
     <Slide
-          easing="ease"
-          duration={7000}
-          indicators   
-          >
-            {
-                images.map(image => {
-                    const url= `/products/${image}`
-                    return (
-                        <div className={styles['each-slide']} key={image}>
-                            <div style={{
-                                backgroundImage: `url(${url})`,
-                                backgroundSize: 'cover'
-                            }}>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-
+      easing="ease"
+      duration={7000}
+      indicators
+    >
+      {
+        images.map(image => {
+          const url = `/products/${image}`
+          return (
+            <div className={styles['each-slide']} key={image}>
+              <div style={{
+                backgroundImage: `url(${url})`,
+                backgroundSize: 'cover'
+              }}>
+              </div>
+            </div>
+          )
+        })
+      }
     </Slide>
   )
 }
-
