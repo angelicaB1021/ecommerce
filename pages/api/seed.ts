12c986a344af5b650de95bfab7771d692c6870d3
rosa angelica beltran <rbeltran3@misena.edu.co>
2024-08-10 14:09:57 -0500

    ay

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { db, seedDatabase } from '../../database';
+import { db, seedDatabase} from '../../database';
 import { Product } from '../../models';
 
 type Data = {
